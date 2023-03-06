f6e7a509b7dd4b549b1f86bee413bf75149ba0db
vishal <vshlbisen2000@gmail.com>
2023-03-06 16:21:25 +0530

    e commerce project added

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+const user = require('./user.js');
+//const login = require('./login.js');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+
+app.use("/user", user);
+app.use(bodyParser.urlencoded({ extended: true }))
+//app.use("/user", login);
+//app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+
