012e4e5c2cebb60b806c93e9e53cf3430cb7d1b2
vishal <vshlbisen2000@gmail.com>
2023-03-21 12:39:31 +0530

    changes in app.js and config.js for getting data from ENV

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const Pool = require('./config.js');
 const app = express();
-const port = 3003;
+const dotenv = require('dotenv');
 
+//const URL = process.env.REACT_APP_BACKEND_URL;
 const user = require('./user.js');
 //const login = require('./login.js');
 const product = require('./product.js')
 const file = require('./file.js')
 const cloths = require('./category.js')
 const bodyParser = require('body-parser')
+dotenv.config({ path: './.env' });
+app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'))
@@ -25,8 +28,8 @@ app.use("/cloth", cloths);
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+app.listen(app.get('port'), () => {
+  console.log(`App running on port ${app.get('port')}.`);
 });
 
 module.exports = Pool;
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
 const pg = require('pg');
-
+require('dotenv').config()
 const Pool = new pg.Pool({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "789456",
-    database: "user"
+    host: process.env.HOST_DATABASE,
+    user: process.env.USER_DATABASE,
+    port: process.env.PORT_DATABASE,
+    password: process.env.PASSWORD_DATABASE,
+    database: process.env.DATABASE_NAME
   });
 
   Pool.connect(function (err) {
