9ed02da29b45be7abca95ba716a87c9642973fb3
Sakshi <sakshimahiskar2000@gmail.com>
2023-04-05 11:58:00 +0530

    changes in get by id

--- product.js
+++ product.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Pool = require('./config.js');
+const { log } = require('console');
 //const app = express();
 //const pg = require('pg');
 
-
-
-
-
 router.get('/', async (req, res) => {
-
-
-
   const allProduct = await Pool.query('SELECT * from products')
   if (allProduct) {
-
     res.send({
       status: true,
       data: allProduct.rows,
@@ -23,35 +16,138 @@ router.get('/', async (req, res) => {
   }
 });
 
+// async function getAllDataById(id) {
+//   try {
+//     const result = await Pool.query('SELECT id, value FROM products WHERE category_id = $1', [c.id]);
+//     if (result.rows.length === 0) {
+//       return null;
+//     }
+//     const products = result.rows[0];
+//     let categorId = null;
+//     if (products.category_id !== null) {
+//       categorId = await getAllDataById( products.category_id);
+//     }
+//     products.parentCategory = categorId;
+//     return products;
+//   }
+//   catch (err) {
+//     console.log(err)
+//   }
+// }
+
+// async function getAllData() {
+//   try {
+//     const result = await Pool.query(`SELECT * FROM category`);
+//     const data = [];
+//     for (const row of result.rows) {
+//       const product = await getAllDataById(row.id);
+//       data.push(product);
+//     }
+//     return data;
+//   } 
+//   catch (err) {
+//     console.log(err)
+//   }
+// }
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const data = await getAllData();
+//     res.send(data);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Server error');
+//   }
+// });
+
 router.get('/:id', async (req, res) => {
-  // console.log(req.params.id);
   const id = req.params.id;
   console.log(Number(id));
   if (!isNaN(Number(id))) {
-    const query = 'SELECT * from products WHERE id = $1';
-    console.log(query);
-    const result = await Pool.query(query, [id]);
-    if (result.rows.length > 0) {
-      res.status(200).send({
+    const product = 
+      `SELECT products.id, products.name, products.price,products.image,products.quantity, category.id AS category_id
+      FROM products
+      INNER JOIN category ON products.category_id = category.id
+      WHERE products.id = $1`;
+    // console.log(product)
+    const data = await Pool.query(product, [id])
+    if (data.rows.length > 0) {
+      res.send({
         status: true,
-        data: result.rows,
+        data: data.rows,
         message: `DATA WITH ID ${id} FETCHED SUCCESFULLY`
-      }
-      )
-    }
-    else {
+      })
+    } else {
+      console.log("data not found")
       res.status(404).send({
         status: false,
         message: `DATA NOT FOUND`
       })
     }
-  }else{
+  }
+  else {
     res.status(404).send({
       status: false,
-      message: `INVALID ID`
+      message: "INVALID ID"
     })
   }
+});
 
+
+// router.get('/prod/:id', async (req, res, next) => {
+//   try {
+//     const id = req.params.id;
+//     const data = await getDataById(id);
+//     if (data) {
+//       res.status(200).json(data.rows);
+//     } else {
+//       res.status(404).json({ message: 'Data not found' });
+//     }
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// router.get('/:id', async (req, res) => {
+//   // console.log(req.params.id);
+//   const id = req.params.id;
+//   console.log(Number(id));
+//   if (!isNaN(Number(id))) {
+//     const query = 'SELECT * from products WHERE id = $1';
+//     console.log(query);
+//     const result = await Pool.query(query, [id]);
+//     if (result.rows.length > 0) {
+//       res.status(200).send({
+//         status: true,
+//         data: result.rows,
+//         message: `DATA WITH ID ${id} FETCHED SUCCESFULLY`
+//       }
+//       )
+//     }
+//     else {
+//       res.status(404).send({
+//         status: false,
+//         message: `DATA NOT FOUND`
+//       })
+//     }
+//   } else {
+//     res.status(404).send({
+//       status: false,
+//       message: `INVALID ID`
+//     })
+//   }
+
+// });
+
+router.get('/products', async (req, res) => {
+  try {
+    const products = await Pool.query(
+      `SELECT p.id, p.name, p.price, p.image,p.quantity, c.name AS category FROM products p INNER JOIN category c ON p.category_id = c.id`
+    );
+    res.json(products.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.post('/', async (req, res) => {
